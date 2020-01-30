b026358d01586265249dfd245d3c0d49761a8731
silvamfpedro <silva.mfpedro@gmail.com>
2020-01-30 17:25:47 +0000

    Added changes in pointnet_cls.py and transform_nets.py files

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -2,11 +2,13 @@
 #   IMPORTS
 # ------------------------
 # Import the necessary packages
-import tf
+import tensorflow as tf
 import numpy as np
 import math
 import sys
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, '../utils'))
+sys.path.append(os.path.join(BASE_DIR, '..\\utils'))
+import utils.tf_util
+
